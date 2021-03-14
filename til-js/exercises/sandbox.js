c0594ebec5aeee82e1e307afc67c273341b1efee
downside154 <blue_tree154@hotmail.com>
2021-03-14 14:55:33 +0900

    second commit

--- til-js/exercises/sandbox.js
+++ til-js/exercises/sandbox.js
@@ -1,5 +1,19 @@
-var x = 15;
+// function numSum(n){
+//     var sum = 0;
+//       for(i = 0; i <= n; i++){
+//         sum += i; 
+//          }
+//     console.log(sum)
+//          }
+// numSum(15);
 
-if (x > 10 && x <20){
-    console.log(x)
+
+var nums;
+var sum = 0;
+
+for (nums = 0; nums < 20; nums++){
+    if ((nums % 3 !== 0) && (nums % 2 !== 0)){
+        sum += nums;
+    }
 }
+console.log(sum)
