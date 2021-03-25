8ae2e17e8158ea201735554ba78b845d7aef4397
downside154 <blue_tree154@hotmail.com>
2021-03-25 17:36:37 +0900

    practiced JS on sandbox and sand

--- til-js/exercises/sandbox.js
+++ til-js/exercises/sandbox.js
@@ -1,21 +1,46 @@
-// var nums;
-// var sum = 0;
 
-// for (nums = 0; nums < 20; nums++){
-//     if ((nums % 3 == 0) || (nums % 2 == 0)){
-//         sum += nums;
-//     }
+// Array.prototype.myforEach = function(fn){
+//   if (typeof fn != 'function'){
+//     throw new TypeError(`${fn} is not a function`);
+//   };
+//   for(i = 0 ; i > this.length, i++) {
+//     fn(this[i], i , this);}};
+
+// [1, 2, 3].myforEach(()=>{})
+
+
+// const numbers = [1, 2, 3];
+// console.log(numbers.map(v => v ** 2))
+// console.log(numbers.filter(v => v > 2))
+
+// Array.prototype.myMap = function(callback) {
+//   if (typeof callback !== 'function'){
+//     throw new TypeError (`${callback} is not a function`);
+//   }
+//   const res = [];
+//   for (let i = 0; i < this.length; i++) {
+//     res.push(callback(this[i], i, this));
+//   }
+//   return res;
+// };
+
+// console.log(numbers); 
+// console.log(numbers.myMap(v => v ** 2)); // [1, 4, 9]
+
+
+
+
+// Array.prototype.myFilter = function(callback) {
+//   if (typeof callback != 'function'){
+//     throw new TypeError (`${callback} is not a function`);
+//   }
+//   const res = [];
+//   for (let i = 0; i < this.length; i++) {
+//     if (callback(this[i], i, this)) res.push(this[i]);
+//   };
+//   return res;
 // }
-// console.log(sum)
 
+// console.log(numbers.myFilter(v => v > 2))
 
-
-var star;
-
-for (star = 10; star > 0; star--){
-        if (star % 2 != 0){
-            var space = ((9 - star)/2);
-            console.log( (" ".repeat(space)) + ("*".repeat(star)) + (" ".repeat(space))) 
-        }
-}
-
+console.log( [1,2,[3],[4]].flat(1))
